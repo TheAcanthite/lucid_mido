95e674c78255ec72ba7f0a9a19a89b7046e69b79
rajadeja <rajsjadeja4@gmail.com>
2020-04-10 17:16:55 +0000

    potato_mido: Add PRODUCT_ARCH
    & use lower res anim

--- potato_mido.mk
+++ potato_mido.mk
@@ -40,3 +40,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+
+PRODUCT_ARCH := arm64
+TARGET_BOOTANIM_LOW_RES := true
+
